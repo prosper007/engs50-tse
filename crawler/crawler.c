58cc97de2c6c8215ad9d30b2fffebc62541a2abd
Mit Patel <Mit.N.Patel.TH@dartmouth.edu>
2020-10-17 19:10:08 -0400

    fixed valgrind for crawler.c

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -8,8 +8,14 @@
  * Description: 
  * 
  */
-#include "webpage.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <curl/curl.h>
+#include <stdbool.h>
+#include "webpage.h"
 
 int main(void) {
 	//create a single new webpage at depth 0, w/ seed URL: https://thayer.github.io/engs50/
@@ -17,18 +23,18 @@ int main(void) {
 	webpage_t *thayer_webpage = webpage_new(url, 0, NULL);
 
 	//check if fetch webpage html to local computer is successful
-	bool fetch = webpage_fetch(*thayer_webpage);
-	if(fetch != TRUE) {
+	bool fetch = webpage_fetch(thayer_webpage);
+	if(fetch != true) {
 		exit(EXIT_FAILURE);
 	}
 	
 	// if successful, scan url
-	char *html = webpage_getHTML(page);
+	//char *html = webpage_getHTML(thayer_webpage);
 	int pos = 0;
 	char *result;
 	while((pos = webpage_getNextURL(thayer_webpage,pos, &result)) > 0) {
 		char* internal;
-		if(IsInternal(result)) {
+		if(IsInternalURL(result)) {
 			internal = "internal";
 		}
 		else {
@@ -38,7 +44,7 @@ int main(void) {
 		free(result);
 	}
 
-	free(html);
-	webpage_delete(page);
+	//free(html);
+	webpage_delete(thayer_webpage);
 	exit(EXIT_SUCCESS);
 }	
