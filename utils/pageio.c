71b71260f02024280dfd02e42e0206b6d1604975
Mit Patel <Mit.N.Patel.TH@dartmouth.edu>
2020-10-25 15:57:18 -0400

    Added pageload to pageio.c

--- utils/pageio.c
+++ utils/pageio.c
@@ -1,20 +1,22 @@
-/* pageio.c --- Module that implements pageio.h
+/* crawler.c --- 
  * 
  * 
- * Author: Tyler Hatch
- * Created: Sun Oct 25 13:22:32 2020 (-0400)
+ * Author: Team Higher Learning
+ * Created: Sat Oct 24 17:18:47 2020 
  * Version: 
  * 
  * Description: 
  * 
  */
-
-#include <stdio.h>                                                                                                                                                                                         
-#include <stdlib.h>                                                                                                                                                                                        
-#include <string.h>                                                                                                                                                                                        
-#include <inttypes.h>                                                                                                                                                                                      
-#include <unistd.h>                                                                                                                                                                                        
-#include "webpage.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <curl/curl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdbool.h>
 #include "pageio.h"
 
 int32_t pagesave(webpage_t *pagep, int id, char *dirname){
@@ -36,5 +38,60 @@ int32_t pagesave(webpage_t *pagep, int id, char *dirname){
 	return 0;
 }
 
-webpage_t *pageload(int id, char *dirnm);
-
+/* 
+ * pageload -- loads the numbered filename <id> in directory <dirnm>
+ * into a new webpage
+ *
+ * returns: non-NULL for success; NULL otherwise
+ */
+webpage_t *pageload(int id, char *dirnm) {
+	//Open file ID at dirnm
+	char full_file_name[50];
+	sprintf(full_file_name, "%s/%d", dirnm, id);
+	FILE *pagefile = fopen(full_file_name, "r");    //"r" bc file is only to be read
+	
+	//Check if pagefile is non-empty
+	if(pagefile == NULL){
+		printf("Requested file is empty\n");
+		return NULL;
+	}
+	
+	//Extract URL from pagefile
+	char url[50];
+	fscanf(pagefile, "%s", url);   //%s tells fscanf to stop after whitespace(new line or tab)
+	
+	//Extract depth from pagefile
+	int depth;
+	fgetc(pagefile);  //skips over space after url line
+    fgetc(pagefile);  //skips over new line after space
+    depth = fgetc(pagefile);  //accesses depth on new line 
+	
+	//Extract HTML from pagefile
+	fgetc(pagefile);	//skips over space after depth line
+	fgetc(pagefile);  //skips over new line afer space 
+	fgetc(pagefile);	//skips over HTML len
+	fgetc(pagefile);  //skips over space after HTML len
+	fgetc(pagefile);  //skips over new line after space
+	int ch;
+	char html[3000];
+	while( (ch=fgetc(pagefile)) != EOF) {
+		ch = (char)ch;
+		strcat(html, ch);
+	}
+	
+	printf("url: %s\n", url);
+	printf("depth: %c\n", depth);
+	printf("HTML: %s\n", html);
+	
+	webpage_t *thayer_webpage = webpage_new(url, depth, html);
+	//char full_file_name[50];
+	//sprintf(full_file_name, "%s/%d", dirname, id);
+	//FILE *pagefile = fopen(full_file_name, "w");
+	return thayer_webpage;
+}
+	
+	
+	
+	
+	
+	
