090e001f9d861a7cfcd994ada9ae9289c07cf1ea
Mit Patel <Mit.N.Patel.TH@dartmouth.edu>
2020-10-25 22:55:58 -0400

    Made indexer for step 2

--- indexer/indexer.c
+++ indexer/indexer.c
@@ -17,7 +17,47 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <stdbool.h>
+#include "pageio.h"
+#include "webpage.h"
+#include <ctype.h>
+
+char* NormalizeWord(char* word) {
+	int str_len = strlen(word);
+	//char* empty_word = "\0";
+	//empty_word[0] = '\0';
+	if(str_len < 3) {
+		//strcpy(word,empty_word);
+		return NULL;
+	}
+	for(int i = 0; i < str_len; i++) {
+		if(isalpha(word[i]) == 0) {
+			//strcpy(word,empty_word);
+			return NULL;
+		}
+		if(isupper(word[i]) != 0) {
+			//strncpy(word[i],tolower(word[i]), 1);
+			//printf("here\n");
+			word[i] = tolower(word[i]);
+		}
+	}
+	return word;
+}	
 
 int main(void) {
-	printf("hello");
+	//Load webpage w/ ID 1
+	webpage_t* loaded_page = pageload(1, "../utils/pages");
+	//print words from HTML
+	char *word;
+	int pos = 0;
+	while((pos = webpage_getNextWord(loaded_page,pos, &word)) > 0) {
+		char* normalized = NormalizeWord(word);
+		//strcpy(word,);
+		if(normalized != NULL) {
+			printf("%s\n", normalized); 
+			//free(word);
+		}
+		free(word);
+	}
+	webpage_delete(loaded_page);
 }
+	
