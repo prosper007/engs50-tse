684c38bad020ed75967f23f3df704892bb254709
Mit Patel <Mit.N.Patel.TH@dartmouth.edu>
2020-10-25 21:11:17 -0400

    started step 2

---
+++ indexer/indexer.c
@@ -0,0 +1,23 @@
+/* indexer.c --- 
+ * 
+ * 
+ * Author: Team Higher Learning
+ * Created: Sun Oct 25 17:18:47 2020 
+ * Version: 
+ * 
+ * Description: 
+ * 
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <curl/curl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdbool.h>
+
+int main(void) {
+	printf("hello");
+}
